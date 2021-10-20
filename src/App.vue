23b762c03bc82fec80ef3155ac138e08522b27f1
LucasR33 <lucasroux72@gmail.com>
2021-10-20 21:03:22 +0200

    Affichage des questions et des reponses

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <link rel="stylesheet" href="./assets/style.scss">
+  <template v-if="this.question"> 
+    <h1 v-html="this.question" class="questionTitle">
+    </h1>
+
+    <template v-for="(answer, index) in this.answers" :key="index">
+      <it-button type="primary" v-html="answer" round></it-button><br>
+    </template>
+    <it-button type="primary" pulse outlined>Envoyer</it-button>
+  </template>
+
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      question: undefined,
+      correctAnswer: undefined,
+      wrongAnswers: undefined
+    }
+  },
+
+  computed: {
+    answers() {
+      var allAnswers = JSON.parse(JSON.stringify(this.wrongAnswers));
+      allAnswers.splice(Math.round(Math.random() * allAnswers.length), 0, this.correctAnswer)
+      return allAnswers
+      }
+  },
+
+  created() {
+    this.axios.get("https://opentdb.com/api.php?amount=1&category=18")
+      .then((response =>{
+        console.log(response.data.results[0])
+        this.question = response.data.results[0].question
+        this.correctAnswer = response.data.results[0].correct_answer
+        this.wrongAnswers = response.data.results[0].incorrect_answers
+      }))
   }
+
 }
 </script>
 
 <style lang="scss">
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 160px auto;
+  
+  .it-btn {
+    margin:20px;
+    display:inline;
+  }
+
+  .it-btn--round {
+    margin: 10px 4px;
+    display:inline;
+    width:300px;
+  }
+  
 }
+
 </style>
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import Equal from 'equal-vue'
+import 'equal-vue/dist/style.css'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)  
+.use(VueAxios, axios)
+.use(Equal) 
+.mount('#app')
+
