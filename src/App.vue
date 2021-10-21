5e90bbf556d04542245fecc2930b32ebee22d39d
LucasR33 <lucasroux72@gmail.com>
2021-10-21 21:08:25 +0200

    Manque plus que le design :(

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,54 @@
 <template>
   
-  <link rel="stylesheet" href="./assets/style.scss">
+  <ScoreBoard :playerScore="this.playerScore" :computerScore="this.computerScore"/>
+
   <template v-if="this.question"> 
     <h1 v-html="this.question" class="questionTitle">
     </h1>
 
-    <template v-for="(answer, index) in this.answers" :key="index">
-      <it-button type="primary" v-html="answer" round></it-button><br>
+    <template v-for="(answer, index) in this.answers" v-bind:key="index">
+      <input 
+      :disabled="this.answerSubmitted"
+      type="radio" 
+      name="options"
+      :value="answer"
+      v-model="this.chosenAnswer">
+
+      <label v-html="answer"></label><br>
     </template>
-    <it-button type="primary" pulse outlined>Envoyer</it-button>
+
+    <it-button v-if="!this.answerSubmitted" type="primary" pulse outlined @click="this.submitAnswer()">Submit</it-button>
+
+    <section v-if="this.answerSubmitted">
+      <it-alert v-if="this.chosenAnswer == this.correctAnswer" iconbox type="success" title="GJ" body="Correct answer"/>
+      <it-alert v-else iconbox type="danger" title="BJ" :body="'Wrong answer ' + this.correctAnswer"/>
+      <it-button type="primary" pulse outlined @click="this.getNewQuestion()">Next Question</it-button>
+    </section>
+  
   </template>
 
 </template>
 
 
-
 <script>
+import ScoreBoard from '@/components/ScoreBoard.vue'
 
 export default {
+
   name: 'App',
+  components: {
+    ScoreBoard
+  },
+
   data() {
     return {
       question: undefined,
       correctAnswer: undefined,
-      wrongAnswers: undefined
+      wrongAnswers: undefined,
+      chosenAnswer: undefined,
+      answerSubmitted: false,
+      playerScore: 0,
+      computerScore: 0
     }
   },
 
@@ -35,14 +60,35 @@ export default {
       }
   },
 
-  created() {
-    this.axios.get("https://opentdb.com/api.php?amount=1&category=18")
+  methods: {
+    submitAnswer() {
+      if(!this.chosenAnswer) {
+        alert("Pick an answer")
+      } else {
+        if(this.chosenAnswer == this.correctAnswer) {
+          this.playerScore ++
+          this.answerSubmitted = true;
+        } else {
+          this.computerScore ++
+          this.answerSubmitted = true;
+        }
+      }
+    },
+    getNewQuestion() {
+      this.answerSubmitted = false
+      this.chosenAnswer = undefined
+      this.question = undefined
+      this.axios.get("https://opentdb.com/api.php?amount=1&category=18")
       .then((response =>{
-        console.log(response.data.results[0])
         this.question = response.data.results[0].question
         this.correctAnswer = response.data.results[0].correct_answer
         this.wrongAnswers = response.data.results[0].incorrect_answers
       }))
+    },
+  },
+
+  created() {
+    this.getNewQuestion()
   }
 
 }
@@ -53,18 +99,11 @@ export default {
 #app {
   text-align: center;
   margin-top: 160px auto;
-  
+
   .it-btn {
     margin:20px;
     display:inline;
   }
-
-  .it-btn--round {
-    margin: 10px 4px;
-    display:inline;
-    width:300px;
-  }
-  
 }
 
 </style>
